eae1c3b1d32cab0ffd3a648ecc619f227f9647b8
wilmichu <wilmermarco@gmail.com>
2024-04-26 18:53:32 -0600

    Hoy hicimmos entrada de variablesy operdores

---
+++ entrada_de_datos.py
@@ -0,0 +1,12 @@
+mi_variable = input('escribe tu valor: ')
+print('tu valor es:' + mi_variable)
+print(type(mi_variable))
+a= input('primer numero:')
+b=input('segundo numero')
+c=int(input('tercer numero'))
+a_transformado = int(a)
+b=int(b)
+resultado =a_transformado+b+c
+print(resultado)
+
+print('resultado: ' + str(resultado))
---
+++ operadore.py
@@ -0,0 +1,14 @@
+print(17%3)
+print(3**2)
+print(24//9)
+a=5
+b=8
+a/=b
+print(a)
+print( 5 == 5 )
+print( 5 != 5 )
+print( 5 > 8 )
+print( 5 < 8 )
+print( 3 <= 10)
+print( 3 >= 10)
+print( 10 >= 10)
