010d5f0b4dea6b40b0ff3d66201c39930bc51a06
wilmichu <wilmermarco@gmail.com>
2024-05-06 21:06:33 -0400

    hoy he actualizado mis ejercicios

--- funciones_2.py
+++ funciones_2.py
@@ -1,17 +1,17 @@
 def porcentaje (numero,porcentaje):
     c=numero*(porcentaje/100)
     return(c)
-dato = input('escribe el primer valor: ')
-dato_2=input('escribe el primer valor: ')
-dato=int(dato)
-dato_2=int(dato_2)
-resultado=porcentaje(dato,dato_2)
-print(resultado)
+#dato = input('escribe el primer valor: ')
+#dato_2=input('escribe el primer valor: ')
+#dato=int(dato)
+#dato_2=int(dato_2)
+#resultado=porcentaje(dato,dato_2)
+#print(resultado)
 
 def porcentaje2 (numero,porcentaje=10):
     resultado=numero*(porcentaje/100)
     return(resultado)
-dato = input('escribe el primer valor: ')
+'''dato = input('escribe el primer valor: ')
 dato_2=input('escribe el primer valor: ')
 dato=int(dato)
 dato_2=int(dato_2)
@@ -19,13 +19,14 @@ resultado=porcentaje(dato,dato_2)
 print(resultado)
 resultado=porcentaje2(numero=80,porcentaje=25)
 print(resultado)
+'''
 
-lista=[20,80,100]
 def aplicacion_porcentaje(lista):
     resultado_porcentaje=[]
     for numero in lista:
         resultado=porcentaje2(numero)
-        resultado_porcentaje.appendÇ(resultado)
+        resultado_porcentaje.append(resultado)
     return resultado_porcentaje
-lista
+lista=[20,80,100]
+imprimir=aplicacion_porcentaje(lista)
 
